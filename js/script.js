9c7b1a5acd769baa1e1ba3a221578950e55c3dbb
Ali <ali@7generationgames.com>
2021-04-28 18:55:49 -0500

    Walk Project

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+//variables go up here
+let img1;
+let img2;
+let img3;
+let sound;
+
+function preload(){
+  img1 = loadImage('assets/tre.png');
+  // img2 = loadImage('assets/done.jpg');
+  // img3 = loadImage('assets/poepol.jpg');
+  sound = loadSound('assets/electricityArc.mp3');
+  //stuff to prepare gets done here
+}
+
+function setup() {
+  createCanvas(1920, 1080);
+//things to establish the "sandbox" go here
+}
+
+function draw() {
+  background(255, 204, 0);
+  image(img1, 0, 0);
+  // image(img2, 450, 300);
+  // image(img3, 1500, 100);
+  //think of this area as a looping function that continuall puts things on screen, in order
+}
+
+function mousePressed() {
+  //what happens when I click?
+  if (sound.isPlaying()) {
+    sound.stop();
+  }
+  else {
+    sound.play();
+    }
+  }
